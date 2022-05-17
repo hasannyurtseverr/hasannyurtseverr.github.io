18ae019da1f94ea44eadbfbce2a45fd085e76ae8
hasannyurtseverr <hasannyurtseverr@gmail.comm>
2022-05-17 17:51:14 +0300

    multi lang yapısına getirildi

--- js/menu.js
+++ js/menu.js
@@ -25,67 +25,108 @@ jQuery(document).ready(function() {
     var arrLang={
         
         'tr':{
-
-            '0':'Chania',
-            'hasan':'Uçuş',
-            '2':'Şehir',
-            '3':'Ada',
-            '4':'Yemek',
-            '5':'Şehir',
-            '6':'Hanya, Girit adasındaki Hanya bölgesinin başkentidir. Şehir iki kısma ayrılabilir, eski şehir ve modern şehir',
-            '7':'Ne?',
-            '8':'Hanya, Girit adasında bir şehirdir.',
-            '9':'Nerede ?',
-            '10':'Girit Mediterranea denizinde bir yunan adasıdır ',
-            '11':"Nasıl?",
-            '12':"Avrupa'nın her yerinden Hanya havalimanına ulaşabilirsiniz.",
-            '13':'İçeriğin yeniden boyutlandırmaya nasıl yanıt verdiğini görmek için tarayıcı penceresini yeniden boyutlandırın.'
- 
-
-
+            'meslek':
+            'hedef':
+            'hedef_paragraf':
+            'dg':
+            'numara':
+            'tecrübeler':
+            'tecrübe 1 başlık':
+            'tecrübe 1 firma':
+            'tecrübe 1 bolum':
+            'tecrübe 1 p':
+            'tecrübe 2 başlık':
+            'tecrübe 2 firma':
+            'tecrübe 2 bolum':
+            'tecrübe 2 p':
+            'tecrübe 3 başlık':
+            'tecrübe 3 firma':
+            'tecrübe 3 bolum':
+            'tecrübe 3 p':
+            'tecrübe 4 başlık':
+            'tecrübe 4 firma':
+            'tecrübe 4 bolum':
+            'tecrübe 4 p':
+            'tecrübe 5 başlık':
+            'tecrübe 5 firma':
+            'tecrübe 5 bolum':
+            'tecrübe 5 p':
+            'Egitim':
+            'Egitim uni':
+            'Egitim bolum':
+            'Egitim derece':
+            'Egitim lise':
         },
 
 
         'en':{
-            '0':'Chania',
-            'hasan':'The Flight',
-            '2':'The City',
-            '3':'The Island',
-            '4':'The Food',
-            '5':'The City',
-            '6':'Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.',
-            '7':'What?',
-            '8':'Chania is a city on the island of Crete.',
-            '9':'Where?',
-            '10':'Crete is a Greek island in the Mediterranean Sea.',
-            '11':'How?',
-            '12':'You can reach Chania airport from all over Europe.',
-            '13':'Resize the browser window to see how the content respond to the resizing.'
+            'meslek':
+            'hedef':
+            'hedef_paragraf':
+            'dg':
+            'numara':
+            'tecrübeler':
+            'tecrübe 1 başlık':
+            'tecrübe 1 firma':
+            'tecrübe 1 bolum':
+            'tecrübe 1 p':
+            'tecrübe 2 başlık':
+            'tecrübe 2 firma':
+            'tecrübe 2 bolum':
+            'tecrübe 2 p':
+            'tecrübe 3 başlık':
+            'tecrübe 3 firma':
+            'tecrübe 3 bolum':
+            'tecrübe 3 p':
+            'tecrübe 4 başlık':
+            'tecrübe 4 firma':
+            'tecrübe 4 bolum':
+            'tecrübe 4 p':
+            'tecrübe 5 başlık':
+            'tecrübe 5 firma':
+            'tecrübe 5 bolum':
+            'tecrübe 5 p':
+            'Egitim':
+            'Egitim uni':
+            'Egitim bolum':
+            'Egitim derece':
+            'Egitim lise':
         },
         
         'de':{
-            '0':'Alm',
-            'hasan':'Was machst du?',
-            '2':'The City',
-            '3':'The Island',
-            '4':'The Food',
-            '5':'The City',
-            '6':'Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.',
-            '7':'What?',
-            '8':'Chania is a city on the island of Crete.',
-            '9':'Where?',
-            '10':'Crete is a Greek island in the Mediterranean Sea.',
-            '11':'How?',
-            '12':'You can reach Chania airport from all over Europe.',
-            '13':'Resize the browser window to see how the content respond to the resizing.'
-        },
-
-
-        
-        
+            'meslek':
+            'hedef':
+            'hedef_paragraf':
+            'dg':
+            'numara':
+            'tecrübeler':
+            'tecrübe 1 başlık':
+            'tecrübe 1 firma':
+            'tecrübe 1 bolum':
+            'tecrübe 1 p':
+            'tecrübe 2 başlık':
+            'tecrübe 2 firma':
+            'tecrübe 2 bolum':
+            'tecrübe 2 p':
+            'tecrübe 3 başlık':
+            'tecrübe 3 firma':
+            'tecrübe 3 bolum':
+            'tecrübe 3 p':
+            'tecrübe 4 başlık':
+            'tecrübe 4 firma':
+            'tecrübe 4 bolum':
+            'tecrübe 4 p':
+            'tecrübe 5 başlık':
+            'tecrübe 5 firma':
+            'tecrübe 5 bolum':
+            'tecrübe 5 p':
+            'Egitim':
+            'Egitim uni':
+            'Egitim bolum':
+            'Egitim derece':
+            'Egitim lise':
+        },            
     };
-
-        
 
     
 $('.dropdown-item').click(function() {
